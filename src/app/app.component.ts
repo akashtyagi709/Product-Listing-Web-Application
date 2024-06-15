7c7c8e41926830c50f57c2e9d0fa7b169b2d82ba
akashtyagi709 <tyagiakash577@gmail.com>
2024-06-15 14:03:06 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'productListingWebApplication';
+
+  constructor(){
+    // https://fakestoreapi.com/products
+  }
+}
